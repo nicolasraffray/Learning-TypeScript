79208c3c7885ebff65515dfde60722c912f95d92
nicolasraffray <nraffray@gmail.com>
2020-04-24 21:00:24 +0100

    first passing test

---
+++ SumOfPos/src/posSum.test.ts
@@ -0,0 +1,10 @@
+import {posSum} from "./posSum"
+import { idText } from "typescript"
+
+describe("PosSum", () => {
+
+  it("returns a positive number", () => {
+    expect(posSum([5])).toEqual(5)
+  }) 
+  
+})
---
+++ SumOfPos/src/posSum.ts
@@ -0,0 +1,5 @@
+
+
+export function posSum(input){
+  return 5
+}
