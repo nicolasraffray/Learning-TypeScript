8d4cdae6973d2d36c327ad02486d885909f3dbbb
nicolasraffray <nraffray@gmail.com>
2020-04-24 21:21:40 +0100

    refactor

--- SumOfPos/src/posSum.ts
+++ SumOfPos/src/posSum.ts
@@ -1,11 +1,5 @@
 
 
 export function posSum(arr:number[]):number{
-  var a = 0
-  for(let i = 0; i < arr.length; i++){
-    if(arr[i] > 0){
-      a += arr[i]
-    } else { a += 0 }
-  }
-  return a
+  return arr.filter((a) => a >= 0).reduce((acc, b) => acc + b, 0)
 }
